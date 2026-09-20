ca9ac20a3a6fbd7f91b840ce9cd9d9bdf93576a7
Kangna Gupta <kangnagupta1225@gmail.com>
2026-09-20 18:41:50 +0530

    Fixed bugs

--- index.js
+++ index.js
@@ -173,22 +173,6 @@ clearCompleted.addEventListener("click", () => {
 
 });
 
-/*themeBtn.addEventListener("click", () => {
-
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "light");
-    }
-
-});
-const savedTheme = localStorage.getItem("theme");
-
-if (savedTheme === "dark") {
-    document.body.classList.add("dark");
-}*/
 const themeToggle = document.getElementById("themeBtn");
 themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark");
